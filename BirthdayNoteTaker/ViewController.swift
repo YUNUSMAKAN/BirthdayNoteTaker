b8c1a497f0552be0a72cd9571ae67272fc33e2dc
Yunus MAKAN <makanyunus@gmail.com>
2020-03-31 18:29:54 +0300

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by MAKAN on 31.03.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
